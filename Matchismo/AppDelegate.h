9cbceca4efce4f2d15e4bef96144cd805efb1c50
jianqun@rocketfuel.com <jianqun@rocketfuel.com>
2014-11-08 20:25:15 -0800

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Jianqun Chen on 11/8/14.
+//  Copyright (c) 2014 Jianqun Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
