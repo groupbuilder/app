9fb34ddc0ad03aeffa578be8de1e0380d70d24c7
jianqun@rocketfuel.com <jianqun@rocketfuel.com>
2014-11-08 23:47:11 -0800

    CardGame Lesson 1

--- Matchismo/ViewController.m
+++ Matchismo/ViewController.m
@@ -7,21 +7,42 @@
 //
 
 #import "ViewController.h"
+#import "Deck.h"
+#import "PlayingCardDeck.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipsCount;
+@property (strong, nonatomic) Deck *deck;
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (Deck *) deck {
+    if (!_deck) {
+        _deck = [[PlayingCardDeck alloc] init];
+    }
+    return _deck;
+}
+- (void) setFlipsCount:(int)flipsCount {
+    _flipsCount = flipsCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipsCount];
+    NSLog(@"Flips %d times", self.flipsCount);
+}
+- (IBAction)touchCardButton:(UIButton *)sender {
+    if ([sender.currentTitle length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    } else {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                          forState:UIControlStateNormal];
+        Card *randomCard = [self.deck drawRandomCard];
+        //[sender setTitle:@"A♣️" forState:UIControlStateNormal];
+        NSLog(@"Choose Card %@", randomCard.contents);
+        [sender setTitle:randomCard.contents forState:UIControlStateNormal];
+    }
+    self.flipsCount++;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
